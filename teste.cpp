d920b32b0a6e8142aeff8139f7614e3a999f602b
AndrePereira03 <andre.pereira@estudante.ufjf.br>
2023-08-24 15:23:57 -0300

    alteracoes

--- teste.cpp
+++ teste.cpp
@@ -7,3 +7,21 @@ int main()
     cout << "Teste!" << endl;
     return 0;
 }
+
+/*
+meu token PAT:
+
+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
+
+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
+
+testeeee
+eeeeee
+eeeeeer
+returnr
+returnr
+return
+returnr
+
+
+*/
